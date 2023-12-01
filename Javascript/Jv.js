4c44fd6cc768061caf84cda84a2c179d930b8280
Gabriele <gabriele.palma.bool@gmail.com>
2023-12-01 16:40:58 +0100

    add function

--- Javascript/Jv.js
+++ Javascript/Jv.js
@@ -58,73 +58,82 @@ const Team = [
 // dichiaro l'elemento che ha per classe box
 const Container = document.querySelector(".row");
 
+function Popolamento(){
 
-for(let i=0; i<Team.length; i++){
+    for(let i=0; i<Team.length; i++){
 
-    //creo una variabile momentanea alla quale acceddo per ottenere informazioni presenti nel mio oggetto
-    let val= Team[i] 
-
-    //Creo l'elemento all'interno del mio file html
-    const Box = document.createElement ("div");
-
-    //aggiungo la classe desiderata
-    Box.classList.add("card");
-
-
-
-    //Creo tag img all'interno del mio file html
-    const img = document.createElement ("img");
-
-    //acquisisco il percorso delle foto e lo passo all'attributo src del tag img
-    img.src = val.foto;
-
-
-
-
-    //Creo l'elemento all'interno del mio file html
-    const BoxT = document.createElement ("div");
-
-    //aggiungo la classe desiderata
-    BoxT.classList.add("card-body");
-
-
-
-
-    //Creo l'elemento all'interno del mio file html
-    const Title = document.createElement ("h5");
-
-    //aggiungo la classe desiderata
-    Title.classList.add("card-title");
-
-    //inserisco scritta all'interno del mio contenuto preso dall'oggetto
-    Title.append(val.nome+" "+val.cognome);
-
-    //inserisco numero all'interno del quadrato il tag h1
-    BoxT.append(Title);
-
-
-    //Creo l'elemento all'interno del mio file html
-    const Text = document.createElement ("p");
-
-    //aggiungo la classe desiderata
-    Text.classList.add("card-text");
-
-    //inserisco scritta all'interno del mio contenuto preso dall'oggetto
-    Text.append(val.ruolo);
-
-    //inserisco numero all'interno del quadrato il tag h1
-    BoxT.append(Text);
-
-
-
-    //inserisco numero all'interno del quadrato il tag h1
-    Box.append(img);
-
-    //inserisco numero all'interno del quadrato il tag h1
-    Box.append(BoxT);
+        //creo una variabile momentanea alla quale acceddo per ottenere informazioni presenti nel mio oggetto
+        let val= Team[i] 
     
-    //inserisco ogni blocco all'inerno del mio container
-    Container.append(Box);
-
-
+        //Creo l'elemento all'interno del mio file html
+        const Box = document.createElement ("div");
+    
+        //aggiungo la classe desiderata
+        Box.classList.add("card");
+    
+    
+    
+        //Creo tag img all'interno del mio file html
+        const img = document.createElement ("img");
+    
+        //acquisisco il percorso delle foto e lo passo all'attributo src del tag img
+        img.src = val.foto;
+    
+    
+    
+    
+        //Creo l'elemento all'interno del mio file html
+        const BoxT = document.createElement ("div");
+    
+        //aggiungo la classe desiderata
+        BoxT.classList.add("card-body");
+    
+    
+    
+    
+        //Creo l'elemento all'interno del mio file html
+        const Title = document.createElement ("h5");
+    
+        //aggiungo la classe desiderata
+        Title.classList.add("card-title");
+    
+        //inserisco scritta all'interno del mio contenuto preso dall'oggetto
+        Title.append(val.nome+" "+val.cognome);
+    
+        //inserisco numero all'interno del quadrato il tag h1
+        BoxT.append(Title);
+    
+    
+        //Creo l'elemento all'interno del mio file html
+        const Text = document.createElement ("p");
+    
+        //aggiungo la classe desiderata
+        Text.classList.add("card-text");
+    
+        //inserisco scritta all'interno del mio contenuto preso dall'oggetto
+        Text.append(val.ruolo);
+    
+        //inserisco numero all'interno del quadrato il tag h1
+        BoxT.append(Text);
+    
+    
+    
+        //inserisco numero all'interno del quadrato il tag h1
+        Box.append(img);
+    
+        //inserisco numero all'interno del quadrato il tag h1
+        Box.append(BoxT);
+        
+        //inserisco ogni blocco all'inerno del mio container
+        Container.append(Box);
+    
+    
+    }
+    
 }
+
+
+Popolamento();
+
+
+
